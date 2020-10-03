90604a932b37498561a8aa8b556395be278db2bc
Davi0k <davidecasale2002@gmail.com>
2020-10-03 21:53:41 +0200

    Update index.js page. Add and update styles and add new sections.
    Add JSX for Git-Hub and Linked-In accounts. Rewrite Partners section.
    Update some styles and some JSX in pages/index.js File. Refactor some
    code and fix some bugs.

--- pages/index.js
+++ pages/index.js
@@ -18,7 +18,7 @@ const Home = (props) => {
     <div className="container max-w-screen-lg mx-auto py-2">
       <Navigation />
 
-      <div className="px-5 py-6">
+      <div className="px-6 py-6">
         <div className="grid grid-cols-3 gap-4">
           <div className="col-span-2 h-40 py-16">
             <h1 className="text-5xl font-semibold">Davide Casale 👋🏻</h1>
@@ -30,22 +30,54 @@ const Home = (props) => {
           </div>
         </div>
 
-        <div id="about" className="py-20">
+        <div id="about" className="mt-20 mb-12">
           <h1 className="text-3xl font-medium mb-2">About Me 🧑🏻</h1>
 
-          <span className="text-lg">
+          <div className="text-lg">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
             Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
             Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
             Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </div>
 
-            <br/><br/>
+          <div className="flex grid grid-cols-6 mt-12">
+            <div className="col-span-1"/>
 
+            <a href="https://www.github.com/Davi0k" target="_blank" className="col-span-2">
+              <div className="w-40 uppercase text-3xl font-mono border-b-8 border-gray-500 text-gray-800 m-auto text-center hover:border-gray-600 hover:text-black">
+                GITHUB
+              </div>
+            </a>  
+
+            <a href="https://www.linkedin.com/in/davide-casale-2002/" target="_blank" className="col-span-2">
+              <div className="w-40 uppercase text-3xl font-mono border-b-8 border-gray-500 text-gray-800 m-auto text-center hover:border-gray-600 hover:text-black">
+                LINKEDIN
+              </div>
+            </a>  
+
+            <div className="grid-cols-1"/>      
+          </div>
+
+          <div className="text-lg mt-12">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
             Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
             Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
             Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-          </span>
+          </div>
+
+          <div className="flex grid grid-cols-10 mt-16">
+            <a href="https://buildyourpath.it" target="_blank" className="col-span-4 mt-5">
+              <img className="w-64 m-auto pointer-events-none" src="partners/byp.png" />
+            </a>   
+
+            <a href="https://pulsing.io" target="_blank" className="col-span-2">
+              <img className="w-24 m-auto pointer-events-none" src="partners/pulsing.png" />
+            </a>   
+
+            <a href="https://aryel.io" target="_blank" className="col-span-4 mt-5">
+              <img className="w-48 m-auto pointer-events-none" src="partners/aryel.png" />
+            </a>       
+          </div>
         </div>
 
         <div id="skills" className="py-4">
@@ -100,21 +132,7 @@ const Home = (props) => {
           <Slider />
         </div>
 
-        <div className="flex grid grid-cols-10 mb-16">
-          <a href="https://buildyourpath.it" target="_blank" className="col-span-4 mt-4">
-            <img className="w-64 m-auto pointer-events-none" src="partners/byp.png" />
-          </a>   
-
-          <a href="https://pulsing.io" target="_blank" className="col-span-2">
-            <img className="w-20 m-auto pointer-events-none" src="partners/pulsing.png" />
-          </a>   
-
-          <a href="https://aryel.io" target="_blank" className="col-span-4 mt-4">
-            <img className="w-40 m-auto pointer-events-none" src="partners/aryel.png" />
-          </a>       
-        </div>
-
-        <div id="contacts" className="mb-10">
+        <div id="contacts" className="mt-6 mb-8">
           <h1 className="text-3xl font-medium mb-2">Contacts & Links 🥼</h1>
 
           <span className="text-lg">
