70a536aebaa1e7f0f786bc3e9f307d628e6d94fe
Davide Casale <davidecasale2002@gmail.com>
2022-10-23 15:04:52 +0200

    Fix bug inside pages/404.tsx error page.

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,9 +1,13 @@
+import React from "react";
+import { useRouter } from "next/router";
+import type { NextRouter } from "next/router";
+
 export default function NotFound() {
+  const router: NextRouter = useRouter();
+
+  React.useEffect(function() {
+    router.replace("/");
+  });
+
   return null;
 }
-
-export const getStaticProps = () => ({
-  redirect: {
-    destination: "/"
-  }
-});
